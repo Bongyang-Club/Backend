7740ff307434f1a1d31209b0b0cc56755591210b
eoguddl <nicenicnic123@gmail.com>
2023-04-24 15:48:26 +0900

    feat: add BasicResponse

--- src/main/java/com/project/bongyang_club_backend/service/member/MemberService.java
+++ src/main/java/com/project/bongyang_club_backend/service/member/MemberService.java
@@ -3,13 +3,14 @@ package com.project.bongyang_club_backend.service.member;
 import com.project.bongyang_club_backend.domain.member.Member;
 import com.project.bongyang_club_backend.dto.SignRequest;
 import com.project.bongyang_club_backend.dto.SignResponse;
+import com.project.bongyang_club_backend.response.BasicResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface MemberService {
 
-    ResponseEntity<SignResponse> login(SignRequest request);
+    ResponseEntity<BasicResponse> login(SignRequest request);
 
     Boolean schoolClubDuplicateCheck(Member member, int type);
 
