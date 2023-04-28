678bf6cb5ee6e2458a533979e7ba6d396378248e
eoguddl <nicenicnic123@gmail.com>
2023-04-28 11:14:25 +0900

    feat: @hidden annotation

--- src/main/java/com/project/bongyang_club_backend/controller/TestController.java
+++ src/main/java/com/project/bongyang_club_backend/controller/TestController.java
@@ -2,12 +2,14 @@ package com.project.bongyang_club_backend.controller;
 
 import com.project.bongyang_club_backend.response.BasicResponse;
 import com.project.bongyang_club_backend.service.test.TestService;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/test")
