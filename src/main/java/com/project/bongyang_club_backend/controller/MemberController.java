f2714cc22c9b23fcfaf5d895d6913a3005905c04
eoguddl <nicenicnic123@gmail.com>
2023-04-28 11:17:33 +0900

    feat: getMemberByToken mapping

--- src/main/java/com/project/bongyang_club_backend/controller/MemberController.java
+++ src/main/java/com/project/bongyang_club_backend/controller/MemberController.java
@@ -1,9 +1,10 @@
 package com.project.bongyang_club_backend.controller;
 
 import com.project.bongyang_club_backend.dto.SignRequest;
-import com.project.bongyang_club_backend.dto.SignResponse;
 import com.project.bongyang_club_backend.response.BasicResponse;
+import com.project.bongyang_club_backend.security.JWTProvider;
 import com.project.bongyang_club_backend.service.member.MemberService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +12,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "Member", description = "Member Management API's")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/login")
+    private final JWTProvider jwtProvider;
+
+    @GetMapping("/member")
+    public ResponseEntity<BasicResponse> getMemberByToken() {
+        return memberService.getMemberByToken();
+    }
+
+    @PostMapping("/login" )
     public ResponseEntity<BasicResponse> login(@RequestBody SignRequest request) {
         return memberService.login(request);
     }
