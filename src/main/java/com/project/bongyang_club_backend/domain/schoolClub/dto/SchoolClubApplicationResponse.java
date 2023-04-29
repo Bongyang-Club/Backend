f93ff890e164094f13119f7d4ba6f81912998bc9
eoguddl <nicenicnic123@gmail.com>
2023-04-29 12:07:42 +0900

    refactor: fix class name dto -> response

--- src/main/java/com/project/bongyang_club_backend/domain/schoolClub/dto/SchoolClubApplicationResponse.java
+++ src/main/java/com/project/bongyang_club_backend/domain/schoolClub/dto/SchoolClubApplicationResponse.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class SchoolClubApplicationDto {
+public class SchoolClubApplicationResponse {
 
     private Long memberJoinId;
 
