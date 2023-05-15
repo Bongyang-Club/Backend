bacb77cb9aefe68dd89991d21d3e660e08cad2c9
eoguddl <nicenicnic123@gmail.com>
2023-05-15 18:30:17 +0900

    refactor: package name

--- src/main/java/com/project/bongyang_club_backend/domain/member/controller/MemberController.java
+++ src/main/java/com/project/bongyang_club_backend/domain/member/controller/MemberController.java
@@ -2,7 +2,6 @@ package com.project.bongyang_club_backend.domain.member.controller;
 
 import com.project.bongyang_club_backend.domain.member.dto.SignRequest;
 import com.project.bongyang_club_backend.global.response.BasicResponse;
-import com.project.bongyang_club_backend.global.security.JWTProvider;
 import com.project.bongyang_club_backend.domain.member.service.MemberService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
